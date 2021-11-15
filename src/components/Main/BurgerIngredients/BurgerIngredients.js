385a775d1983930ffd376f2a6bcf43448e91e286
Irina Abah <irin.bon@gmail.com>
2021-11-15 17:08:47 +0000

    style: styles for ingredients

--- src/components/Main/BurgerIngredients/BurgerIngredients.js
+++ src/components/Main/BurgerIngredients/BurgerIngredients.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { HashLink as Link } from "react-router-hash-link";
-import Ingredients from "../BurgerIngredients/BurgerIngredients.module.css";
+import IngredientsStyles from "../BurgerIngredients/BurgerIngredients.module.css";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import Ingredient from "../Ingredient/Ingredient";
 import data from "../../../utils/data"
@@ -17,40 +17,43 @@ function BurgerIngredients() {
     <div>
       <h1 className={`text text_type_main-large mb-5`}>Соберите бургер</h1>
       <div style={{ display: 'flex' }}>
-        <Tab value="one" active={current === 'one'} onClick={setCurrent}><Link className={Ingredients.tab} to="#buns">Булки</Link>
+        <Tab value="one" active={current === 'one'} onClick={setCurrent}><Link className={IngredientsStyles.tab} to="#buns">Булки</Link>
         </Tab>
-        <Tab value="two" active={current === 'two'} onClick={setCurrent}><Link className={Ingredients.tab} to="#sauses">Соусы</Link>
+        <Tab value="two" active={current === 'two'} onClick={setCurrent}><Link className={IngredientsStyles.tab} to="#sauses">Соусы</Link>
         </Tab>
-        <Tab value="three" active={current === 'three'} onClick={setCurrent}><Link className={Ingredients.tab} to="#mains">Начинки</Link>
+        <Tab value="three" active={current === 'three'} onClick={setCurrent}><Link className={IngredientsStyles.tab} to="#mains">Начинки</Link>
         </Tab>
       </div>
-      <div>
-        <h2 id="buns">Булки</h2>
-        <div>
-          {buns.map((item, i) => (
-            <Ingredient item={item} key={item._id}
-            />
-          ))}
+      <div className={IngredientsStyles.menu}>
+        <div className={`mt-10`}>
+          <h2 className={`${IngredientsStyles.title} text text_type_main-medium`} id="buns">Булки</h2>
+          <div className={`${IngredientsStyles.type} mr-4 ml-4 mt-6`}>
+            {buns.map((item, i) => (
+              <Ingredient item={item} key={item._id}
+              />
+            ))}
+          </div>
+        </div>
+        <div className={`mt-10`}>
+          <h2 className={`${IngredientsStyles.title} text text_type_main-medium`} id="sauses">Соусы</h2>
+          <div className={`${IngredientsStyles.type} mr-4 ml-4 mt-6`}>
+            {sauses.map((item, i) => (
+              <Ingredient item={item} key={item._id}
+              />
+            ))}
+          </div>
+        </div>
+        <div className={`mt-10`}>
+          <h2 className={`${IngredientsStyles.title} text text_type_main-medium`} id="mains">Начинки</h2>
+          <div className={`${IngredientsStyles.type} mr-4 ml-4 mt-6`}>
+            {mains.map((item, i) => (
+              <Ingredient item={item} key={item._id}
+              />
+            ))}
+          </div>
         </div>
       </div>
-      <div>
-        <h2 id="sauses">Соусы</h2>
-        <div>
-          {sauses.map((item, i) => (
-            <Ingredient item={item} key={item._id}
-            />
-          ))}
-        </div>
-      </div>
-      <div>
-        <h2 id="mains">Начинки</h2>
-        <div>
-          {mains.map((item, i) => (
-            <Ingredient item={item} key={item._id}
-            />
-          ))}
-        </div>
-      </div>
+      
     </div>
   )
 }
--- src/components/Main/Ingredient/Ingredient.js
+++ src/components/Main/Ingredient/Ingredient.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { CurrencyIcon, Counter } from "@ya.praktikum/react-developer-burger-ui-components";
 import PropTypes from "prop-types";
+import IngredientStyles from "../Ingredient/Ingredient.module.css";
 
 function Ingredient({item}) {
 
   return (
-    <div>
+    <div className={`${IngredientStyles.ingredient}`}>
       <img src={item.image} alt="Изображение продукта"/>
-      <div>
-        <p>{item.price}</p>
+      <div className={`${IngredientStyles.price} mt-1 mb-1`}>
+        <p className={`text text_type_digits-default`}>{item.price}</p>
         <CurrencyIcon type="primary" />
       </div>
-      <h2>{item.name}</h2>
+      <h2 className={`${IngredientStyles.title} text text_type_main-default`}>{item.name}</h2>
       <Counter count={1} size="default" />
     </div>
   )
