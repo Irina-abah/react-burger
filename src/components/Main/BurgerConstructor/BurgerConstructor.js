b59347887ab852700a4e6ec3e772827b0b10b308
Irina Abah <irin.bon@gmail.com>
2021-11-17 14:44:44 +0000

    style: fixed scrolls

--- src/components/Main/BurgerConstructor/BurgerConstructor.js
+++ src/components/Main/BurgerConstructor/BurgerConstructor.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ConstructorStyles from "../BurgerConstructor/BurgerConstructor.module.css";
-import { ConstructorElement, DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { ConstructorElement, DragIcon, CurrencyIcon, Button } from "@ya.praktikum/react-developer-burger-ui-components";
 import data from "../../../utils/data";
 
 function BurgerConstructor() {
@@ -8,40 +8,47 @@ function BurgerConstructor() {
   const addedItems = data.filter((item) => item.type !== 'bun')
 
   return (
-    <div className={`${ConstructorStyles.wrapper} mt-15`}>
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-      <ConstructorElement
-        type="top"
-        isLocked={true}
-        text={`${data[0].name} (верх)`}
-        price={data[0].price}
-        thumbnail={data[0].image}
-      />
+    <div className={`mt-15`}>
+      <div className={`${ConstructorStyles.wrapper} mb-10 pr-2 pl-4`}>
+        <ConstructorElement
+          type="top"
+          isLocked={true}
+          text={`${data[0].name} (верх)`}
+          price={data[0].price}
+          thumbnail={data[0].image}
+        />
+        <div className={`${ConstructorStyles.food_list} pr-2 pl-4`}>
+          {addedItems.map((item, i) => (
+            <div className={ConstructorStyles.food_item}>
+              <DragIcon type="primary" />
+              <ConstructorElement
+                key={item._id}
+                text={item.name}
+                price={item.price}
+                thumbnail={item.image}
+              />
+            </div>
+          ))}
+        </div>
+        <ConstructorElement
+          type="bottom"
+          isLocked={true}
+          text={`${data[0].name} (низ)`}
+          price={data[0].price}
+          thumbnail={data[0].image}
+        />
       </div>
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-        {addedItems.map((item, i) => (
-          <div className={ConstructorStyles.food}>
-            <DragIcon type="primary" />
-            <ConstructorElement
-              key={item._id}
-              text={item.name}
-              price={item.price}
-              thumbnail={item.image}
-            />
-          </div>
-        ))}
+      <div className={`${ConstructorStyles.order}`}>
+        <span className={`${ConstructorStyles.price}`}>
+          <p className={`text text_type_digits-medium`}>999</p>
+          <CurrencyIcon type="primary" />
+        </span>
+        <Button type="primary" size="large">
+          Оформить заказ 
+        </Button>
       </div>
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-      <ConstructorElement
-        type="bottom"
-        isLocked={true}
-        text={`${data[0].name} (низ)`}
-        price={data[0].price}
-        thumbnail={data[0].image}
-      />
-      </div>
-
     </div>
+    
     
   )
 }
