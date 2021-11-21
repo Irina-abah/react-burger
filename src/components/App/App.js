abe8e932640ac1e66b32557e27a6ac1d7b834274
Irina Abah <irin.bon@gmail.com>
2021-11-21 22:33:22 +0000

    feat: add api

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,28 @@ import React from 'react';
 import MainStyles from "./App.module.css";
 import Header from "../AppHeader/AppHeader";
 import Main from "../Main/Main";
-import ingredients from "../../utils/data";
+// import ingredients from "../../utils/data";
+import allIngredientsApi from "../../utils/MainApi";
 
 function App() {
+
+  const [foodData, setFoodData] = React.useState([]);
+
+  React.useEffect(() => {
+    allIngredientsApi.getIngredients()
+    .then((data) => {
+      console.log(data)
+      setFoodData(data)
+    .catch((err) => {
+      console.log(err);
+      })
+    })
+  }, [])
+
   return (
     <div className={MainStyles.App}>
       <Header />
-      <Main data={ingredients}/>
+      <Main data={foodData}/>
     </div>
   )
 }
---
+++ src/utils/MainApi.js
@@ -0,0 +1,35 @@
+import {BASE_URL} from "../utils/constants";
+
+export class IngredientsApi {
+  constructor({address, headers}) {
+    this._address = address;
+    this._headers = headers;
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+        return res.json()
+    }
+
+    return Promise.reject(`Error ${res.status}`)
+  }
+
+  getIngredients() {
+    return fetch(`${this._address}`, {
+        headers: this._headers
+    })
+    .then((res) => {
+        return this._checkResponse(res)
+    })
+  }
+
+}
+
+const allIngredientsApi = new IngredientsApi({
+  address: BASE_URL,
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
+
+export default allIngredientsApi;
