6a3b92287d3df1605fccf2f970f606f2e650ce67
Irina Abah <irin.bon@gmail.com>
2021-11-23 22:38:36 +0000

    fix: review-2 fix with uppercase

---
+++ src/components/main/main.js
@@ -0,0 +1,21 @@
+import mainStyles from "./main.module.css";
+import BurgerConstructor from "../burder-constructor/burder-constructor";
+import BurgerIngredients from "../burger-ingredients/burger-ingredients";
+import ingredientType from "../../utils/types";
+import PropTypes from "prop-types";
+
+function Main({data, isFailed}) {
+  return (
+    <section className={`${mainStyles.main} pl-5 pr-5 pb-10 pt-10`}>
+      <BurgerIngredients data={data} isFailed={isFailed}/>
+      <BurgerConstructor data={data}/>
+    </section>
+  )
+}
+
+Main.propTypes = {
+  data: PropTypes.arrayOf(ingredientType).isRequired,
+  isFailed: PropTypes.bool.isRequired
+}
+
+export default Main;
