60c6ae97454f73a53dd08aa45977ca98b6e555bc
Irina Abah <irin.bon@gmail.com>
2022-04-15 12:56:24 +0100

    feat: added action for order

---
+++ src/services/actions/order.js
@@ -0,0 +1,43 @@
+import { BASE_URL } from "../../utils/constants";
+
+export const MAKE_ORDER = 'MAKE_ORDER';
+export const MAKE_ORDER_SUCCESS = "MAKE_ORDER_SUCCESS";
+export const MAKE_ORDER_FAILED = "MAKE_ORDER_FAILED";
+
+export const makeOrder = (data) => {
+  return function (dispatch) {
+    dispatch({
+      type: MAKE_ORDER
+    })
+    fetch(`${BASE_URL}/orders`, {
+      method: "POST",
+      body: JSON.stringify({
+        ingredients: data
+      }),
+    })
+    .then((res) => {
+      if (res.ok) {
+        return res.json()
+      }
+      return Promise.reject(`Error ${res.status}`)
+    })
+    .then((res) => {
+      if (res && res.success) {
+        dispatch({
+          type: MAKE_ORDER_SUCCESS,
+          number: res.order.number
+        })
+      } else {
+        dispatch({
+          type: MAKE_ORDER_FAILED,
+        })
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+      dispatch({
+        type: MAKE_ORDER_FAILED,
+      })
+    })
+  }
+}
