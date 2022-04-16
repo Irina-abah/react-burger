29a578da9b73e70ab2eb5067850569a3537737c5
Irina Abah <irin.bon@gmail.com>
2022-04-16 12:15:20 +0100

    feat: added order reducer

---
+++ src/services/reducers/order.js
@@ -0,0 +1,40 @@
+import {
+  MAKE_ORDER,
+  MAKE_ORDER_SUCCESS,
+  MAKE_ORDER_FAILED
+} from "../actions/order";
+
+const initialState = {
+  order: null,
+  orderRequest: false,
+  orderFailed: false,
+}
+
+export const makeOrder = (state = initialState, action) => {
+  switch (action.type) {
+    case MAKE_ORDER: {
+      return {
+        ...state,
+        orderRequest: true
+      }
+    }
+    case MAKE_ORDER_SUCCESS: {
+      return {
+        ...state,
+        order: action.number,
+        orderRequest: false,
+        orderFailed: false,
+      }
+    }
+    case MAKE_ORDER_FAILED: {
+      return {
+        ...state,
+        orderFailed: true,
+        orderRequest: false,
+      }
+    }
+    default: {
+      return state;
+    }
+  }
+}
