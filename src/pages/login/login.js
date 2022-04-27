7e20bbe81e8c2718f59537a8509134b97abf1e3f
Irina Abah <irin.bon@gmail.com>
2022-04-27 23:17:05 +0100

    style: styles for login page finished

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,23 +1,62 @@
-import UserEntry from "../user-entry/user-entry";
+import { useState } from 'react';
+import UserForm from "../user-form/user-form";
+import loginStyles from "./login.module.css";
+import { Link } from "react-router-dom";
+import { PasswordInput, EmailInput } from '@ya.praktikum/react-developer-burger-ui-components';
 
 function Login() {
 
-  const handleSubmit = () => {
+  const [state, setState] = useState({
+    email: "",
+    password: ""
+  });
 
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
   }
   
+  const handleInputChange = (e) => {
+      const value = e.target.value;
+      const name = e.target.name;
+  
+      setState({
+        ...state,
+        [name]: value
+      })
+    }
+
   return (
-    <section>
-      <UserEntry
+    <section className={loginStyles.login}>
+      <UserForm
         title="Вход"
         onSubmit={handleSubmit}
         buttonName="Войти"
-        message="Вы — новый пользователь?" 
+        message="Вы — новый пользователь? " 
         link="/register" 
         linkName="Зарегистрироваться"
       >
-
-      </UserEntry>
+        <div className={`mb-6`}>
+          <EmailInput 
+            onChange={handleInputChange} 
+            value={state.email} 
+            name={'email'}
+          />
+        </div> 
+        <div className={`mb-6`}>
+          <PasswordInput 
+            onChange={handleInputChange} 
+            value={state.password} 
+            name={'password'}
+          />
+        </div>
+      </UserForm>
+      <p className={`text text_type_main-default text_color_inactive`}>
+        {"Забыли пароль? "}
+        <Link to="/forgot-password" className={`text text_type_main-default`}>
+        Восстановить пароль
+        </Link>
+      </p>
     </section>
   )
 }
---
+++ src/pages/user-form/user-form.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import userStyles from "./user-form.module.css";
+import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
+
+function UserForm({title, onSubmit, children, buttonName, message, link, linkName}) {
+
+  return (
+    <form 
+      className={userStyles.form} 
+      name="user-entry" 
+      onSubmit={onSubmit}>
+        <h1 className={`${userStyles.title} text text_type_main-medium mb-6`}>{title}</h1>
+        <div className={userStyles.container}>
+          {children}
+        </div>
+        <div className={`mb-20`}>
+          <Button type="primary" size="medium">
+            {buttonName}
+          </Button>
+        </div>
+        <p className={`text text_type_main-default text_color_inactive mb-4`}>
+        {message} 
+        <Link to={link} className={`text text_type_main-default`}>
+          {linkName}
+        </Link>
+      </p>
+    </form>
+  )
+};
+
+export default UserForm;
