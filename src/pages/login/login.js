93535c0e66d8651a711397dfa0f4f7a61e211144
Irina Abah <irin.bon@gmail.com>
2022-05-10 17:24:03 +0100

    fix: fixed location.state for login page

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,7 +1,7 @@
 import { useState, useCallback } from 'react';
 import UserForm from "../user-form/user-form";
 import loginStyles from "./login.module.css";
-import { Link, Redirect } from "react-router-dom";
+import { Link, Redirect, useLocation } from "react-router-dom";
 import { PasswordInput, EmailInput } from '@ya.praktikum/react-developer-burger-ui-components';
 import { useSelector, useDispatch } from 'react-redux';
 import { loginUser } from "../../services/actions/login";
@@ -9,6 +9,7 @@ import { loginUser } from "../../services/actions/login";
 function Login() {
 
   const dispatch = useDispatch();
+  const location = useLocation()
   const auth = useSelector((store) => store.login.isAuthenticated);
   const [state, setState] = useState({
     email: "",
@@ -37,7 +38,7 @@ function Login() {
     if (auth) {
       return (
         <Redirect
-          to={ state?.from || '/' }
+          to={ location?.state?.from || '/' }
         />
       );
     }
