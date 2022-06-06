9bc9a3823df4d904144f48f28e92edb1ab9f3f4c
Irina Abah <irin.bon@gmail.com>
2022-06-06 22:39:47 +0100

    feat: add types to ingredient modal

--- src/services/actions/ingredient-modal.ts
+++ src/services/actions/ingredient-modal.ts
@@ -1,2 +1,16 @@
-export const OPEN_MODAL: "SHOW_MODAL" = "SHOW_MODAL";
+import { TItem } from "../../utils/types";
+
+export const OPEN_MODAL: "OPEN_MODAL" = "OPEN_MODAL";
 export const CLOSE_MODAL: "CLOSE_MODAL" = "CLOSE_MODAL";
+export interface IOpenModalAction {
+  readonly type: typeof OPEN_MODAL;
+  item: TItem
+};
+export interface ICloseModalAction {
+  readonly type: typeof CLOSE_MODAL;
+  item: {};
+}
+
+export type TModalActions = 
+  | IOpenModalAction
+  | ICloseModalAction;
--- src/services/reducers/ingredient-modal.ts
+++ src/services/reducers/ingredient-modal.ts
@@ -2,14 +2,22 @@ import {
   OPEN_MODAL,
   CLOSE_MODAL
 } from "../actions/ingredient-modal";
+import { TModalActions} from '../actions/ingredient-modal';
+import { TItem } from "../../utils/types";
 
-const initialState = {
+type TIngredientState = {
+  selectedItem: TItem | {};
+  modalOpened: boolean;
+  modalClosed: boolean;
+}
+
+const initialState: TIngredientState = {
   selectedItem: {},
   modalOpened: false,
   modalClosed: true,
 }
 
-export const modalReducer = (state = initialState, action) => {
+export const modalReducer = (state = initialState, action: TModalActions) => {
   switch (action.type) {
     case OPEN_MODAL: {
       return {
